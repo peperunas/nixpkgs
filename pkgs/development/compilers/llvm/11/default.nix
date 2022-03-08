170b5edebc65377ba7030d24942f796389d7facb
Giulio De Pasquale <depasquale+git@giugl.io>
2022-03-08 17:43:17 +0100

    Backport LLVM 13 structure to LLVM 13 toolchain

--- pkgs/development/compilers/llvm/11/default.nix
+++ pkgs/development/compilers/llvm/11/default.nix
@@ -30,7 +30,7 @@ let
     owner = "llvm";
     repo = "llvm-project";
     rev = if rev != "" then rev else "llvmorg-${version}";
-    sha256 = "c7k3frg72wrldnbxvrjfp3sbvpsbn55l";
+    sha256 = "199yq3a214avcbi4kk2q0ajriifkvsr0l2dkx3a666m033ihi1ff";
   };
 
   llvm_meta = {
